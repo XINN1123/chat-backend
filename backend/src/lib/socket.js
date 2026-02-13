1bed189e99c61004b493f004012d55e33251599e
CH LUWANGLEMBA SINGH <165910799+XINN1123@users.noreply.github.com>
2026-02-14 01:11:38 +0530

    Add files via upload

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,51 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://chat-frontend-d1l89ru9o-xinn1123s-projects.vercel.app/"
+  ],
+  credentials: true
+}));
+
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "https://chat-frontend-d1l89ru9o-xinn1123s-projects.vercel.app/"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
+  },
+});
+
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
+
+// used to store online users
+const userSocketMap = {}; // {userId: socketId}
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
+
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+export { io, app, server };
